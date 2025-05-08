091aa04608093b085a5e62c04492d56ab3c1825e
Ayush Verma <ayushv2909@gmail.com>
2025-05-08 21:18:27 +0530

    adding correct path name for styles

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(
   })
 );
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
