d11c13cee5a3cbf6e4aef7095d1b0a8361e3abae
Ayush Verma <ayushv2909@gmail.com>
2025-05-10 01:45:58 +0530

    env confg and callbackurl

--- api/auth.js
+++ api/auth.js
@@ -219,7 +219,7 @@ passport.use(
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/authuser/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if user exists
